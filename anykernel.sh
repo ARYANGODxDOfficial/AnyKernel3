97cb5bf8ddd586f6b5d767f3bd94e18f42581b38
Shoiya Akari <akariondev@gmail.com>
2025-01-14 22:01:08 -0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,30 +1,48 @@
-### MoeKernel - Xiaomi Redmi Note 8
-## Akari Azusagawa @ github-dev
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
 properties() { '
-kernel.string=MoeKernel by whyakari
-do.devicecheck=1
+kernel.string=MoeKernel by @whyakari
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
-supported.versions=11.0-15.0
+device.name1=maguro
+device.name2=toro
+device.name3=toroplus
+device.name4=tuna
+device.name5=
+supported.versions=
 supported.patchlevels=
+supported.vendorpatchlevels=
 '; }
 
-attributes() {
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $RAMDISK/*;
+set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 }
 
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
-ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+BLOCK=auto;
+IS_SLOT_DEVICE=0;
+RAMDISK_COMPRESSION=auto;
+PATCH_VBMETA_FLAG=auto;
 
-. tools/ak3-core.sh && attributes;
+. tools/ak3-core.sh;
 
 dump_boot;
+
+backup_file init.rc;
+replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+
+backup_file init.tuna.rc;
+insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+append_file init.tuna.rc "bootscript" init.tuna;
+
+backup_file fstab.tuna;
+patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+append_file fstab.tuna "usbdisk" fstab;
+
 write_boot;
