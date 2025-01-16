6e37a9564941142b3a24d625297ee8bae3aca191
Shoiya Akari <akariondev@gmail.com>
2025-01-15 23:06:43 -0300

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -1,17 +1,19 @@
-### MoeKernel - Xiaomi Redmi Note 8
-## Akari Azusagawa @ github-dev
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
 
 properties() { '
-kernel.string=MoeKernel by whyakari
+kernel.string=MoeKernel by @whyakari
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
-supported.versions=11.0-15.0
-supported.patchlevels=
+device.name1=java
+device.name2=p352
+device.name3=ums512
+device.name4=moto g20 
+supported.versions=11-12-13-14
+supported.patchlevels=2021-01 -
 '; }
 
 attributes() {
@@ -19,10 +21,10 @@ set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 }
 
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+block=auto
+is_slot_device=1;
 ramdisk_compression=auto;
-patch_vbmeta_flag=auto;
+patch_vbmeta_flag=0;
 
 . tools/ak3-core.sh && attributes;
 
